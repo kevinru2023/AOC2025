ba67c976f1a940f48ceca82c1a7ab25121200ffd
Kevin R <kevinru2005@gmail.com>
2025-12-01 16:54:29 -0800

    Updated Sol 1 for day 1 part 2

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,18 +1,27 @@
 use aoc2025::utils;
-
 fn main(){
     let mut curr_pos: i32 = 50;
-    let mut password: u32 = 0;
+    let mut password: i32 = 0;
     
     let v: Vec<String> = utils::read_lines(1);
     for line in &v{
-        let mut num: i32 = line[1..line.len()].parse::<i32>().unwrap(); 
-        if line.starts_with("L"){
-            num *= -1;
-        }
-        curr_pos = (curr_pos + num) % 100;
-        if curr_pos == 0 {
-            password += 1;
+        let num: i32 = line[1..line.len()].parse::<i32>().unwrap(); 
+        for _ in 0..num {
+            if line.starts_with("L"){
+                curr_pos -= 1;
+            } else{
+                curr_pos += 1;
+            }
+            
+            // check bounds
+            if curr_pos == -1 {
+                curr_pos = 99;
+            } else if curr_pos == 100 {
+                curr_pos = 0;
+            }
+            if curr_pos == 0 {
+                password += 1;
+            }
         }
     }
     println!("Password: {}", password);
