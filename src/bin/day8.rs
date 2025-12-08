d847f36915d0f471db6265b18a61a0cc8162411f
Kevin R <kevinru2005@gmail.com>
2025-12-08 01:40:42 -0800

    my cs325 teacher smirking knowing i'm putting the graph algos to use

---
+++ src/bin/day8.rs
@@ -0,0 +1,156 @@
+use aoc2025::utils;
+use std::{collections::BinaryHeap, env, fmt};
+
+#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd)]
+struct Point {
+    x: u32,
+    y: u32,
+    z: u32,
+}
+
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {}, {})", self.x, self.y, self.z)
+    }
+}
+
+// rust typing is so fun
+fn distance(p1: &Point, p2: &Point) -> i64 {
+    let dx = (p1.x as i64 - p2.x as i64).pow(2);
+    let dy = (p1.y as i64 - p2.y as i64).pow(2);
+    let dz = (p1.z as i64 - p2.z as i64).pow(2);
+    ((dx + dy + dz) as f64).sqrt().round() as i64
+}
+
+fn to_point_vec(input: &Vec<String>) -> Vec<Point> {
+    input
+        .iter()
+        .map(|line| {
+            let parts: Vec<&str> = line.split(',').collect();
+            // turbo fish on top
+            let x = parts[0].parse::<u32>().unwrap();
+            let y = parts[1].parse::<u32>().unwrap();
+            let z = parts[2].parse::<u32>().unwrap();
+            Point { x, y, z }
+        })
+        .collect()
+}
+
+// this func will take in a vector of points and
+// return the 1000 closest pairs as edges (idx pairs)
+// to allow make it easier to perform union-find operations
+fn create_graph(points: &Vec<Point>) -> Vec<(usize, usize)> {
+    let mut heap: BinaryHeap<(i64, (usize, usize))> = BinaryHeap::new();
+    for i in 0..points.len() {
+        for j in i + 1..points.len() {
+            let dist = distance(&points[i], &points[j]);
+            heap.push((-dist, (i, j)));
+        }
+    }
+    let mut edges = Vec::new();
+    let mut count = 0;
+    while count < 1000 && !heap.is_empty() {
+        // NOTE: swap this to 1000 later
+        let (_, (p1, p2)) = heap.pop().unwrap();
+        edges.push((p1, p2));
+        count += 1;
+    }
+    edges
+}
+
+// Part 1 problem:
+// given a bunch of points in a 3D graph want to connect together the points that are closet to each other
+// based of their straight line distance. Want to connect the 1000 closest pair of points together and want
+// to multiply the size of the 3 largest islands together.
+//
+// Idea:
+// So lets break this down into smaller problems
+// 1. First let's find out the closest pair of points and put them in a min heap
+// 2. Then let's find out the 1000 closest pair of points by just popping 1000 items of the heap
+// 3. Finally let's find out the size of the 3 largest islands and multiply them together ??
+
+fn part_1(edges: Vec<(usize, usize)>, num_points: usize) {
+    let mut dsu = utils::DSU::new(num_points);
+
+    // Connect all the edges
+    for (u, v) in &edges {
+        dsu.union(*u, *v);
+    }
+
+    // Find all unique component sizes
+    let mut sizes: BinaryHeap<usize> = BinaryHeap::new();
+    let mut seen_roots = std::collections::HashSet::new();
+
+    for i in 0..num_points {
+        let root = dsu.find(i);
+        if seen_roots.insert(root) {
+            sizes.push(dsu.component_size(root));
+        }
+    }
+
+    // Multiply the 3 largest component sizes
+    let mut res: u64 = 1;
+    for _ in 0..3 {
+        if let Some(size) = sizes.pop() {
+            res *= size as u64;
+        }
+    }
+    println!("Part 1: {}", res);
+}
+
+// modified create_graph func (also at this point I realized I could have just sorted the edges but whatever it's getting late, wait maybe not?)
+fn create_graph_part2(points: &Vec<Point>) -> Vec<(usize, usize)> {
+    let mut heap: BinaryHeap<(i64, (usize, usize))> = BinaryHeap::new();
+    for i in 0..points.len() {
+        for j in i + 1..points.len() {
+            let dist = distance(&points[i], &points[j]);
+            heap.push((-dist, (i, j)));
+        }
+    }
+    let mut edges = Vec::new();
+    while !heap.is_empty() {
+        // NOTE: swap this to 1000 later
+        let (_, (p1, p2)) = heap.pop().unwrap();
+        edges.push((p1, p2));
+    }
+    edges
+}
+
+// Part 2 problem:
+// Continue connecting the closest unconnected pairs of points together until they're all in the same component
+// then return the last two x coordinates of the last two points multiplied
+//
+// Idea:
+// This is way easier than the last one since most of the logic is already done, we just need to refine
+// our graph a bit to instead just contain all edges in a sorted order and then we will use our built in
+// lib function to see the size of our current component
+
+fn part_2(edges: Vec<(usize, usize)>, num_points: usize, points: &Vec<Point>) {
+    let mut uf = utils::DSU::new(num_points);
+    let mut last_pair = (0, 0);
+
+    for (p1, p2) in edges {
+        // Check if already connected (same component)
+        if !uf.connected(p1, p2) {
+            uf.union(p1, p2);
+            if uf.component_size(p1) == num_points {
+                last_pair = (p1, p2);
+                break;
+            }
+        }
+    }
+    let res: u64 = points[last_pair.0].x as u64 * points[last_pair.1].x as u64; // overflow was causing me a headache so we ball 
+    println!("Part 2: {}", res);
+}
+
+fn main() {
+    env::set_var("RUST_BACKTRACE", "1");
+    let input: Vec<String> = utils::read_lines(8);
+    let points = to_point_vec(&input);
+    // for (point, idx) in points.iter().enumerate() {
+    //     println!("Point: {:?} | idx: {}", point, idx);
+    // }
+    let graph = create_graph(&points);
+    part_1(graph, points.len());
+    part_2(create_graph_part2(&points), points.len(), &points);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -12,4 +12,48 @@ pub mod utils {
             .map(|s| s.to_string())
             .collect()
     }
+    
+    // for day 8, SO AI cause this was a headache (mainly union func)
+    pub struct DSU {
+        parent: Vec<usize>,
+        size: Vec<usize>,
+    }
+    
+    impl DSU {
+        pub fn new(n: usize) -> Self {
+            Self {
+                parent: (0..n).collect(),
+                size: vec![1; n],
+            }
+        }
+    
+        pub fn find(&mut self, x: usize) -> usize {
+            if self.parent[x] != x {
+                let root = self.find(self.parent[x]);
+                self.parent[x] = root;
+            }
+            self.parent[x]
+        }
+    
+        pub fn union(&mut self, a: usize, b: usize) {
+            let mut root_a = self.find(a);
+            let mut root_b = self.find(b);
+            if root_a != root_b {
+                if self.size[root_a] < self.size[root_b] {
+                    std::mem::swap(&mut root_a, &mut root_b); // wow.
+                }
+                self.parent[root_b] = root_a;
+                self.size[root_a] += self.size[root_b];
+            }
+        }
+        
+        pub fn connected(&mut self, a: usize, b: usize) -> bool {
+            self.find(a) == self.find(b)
+        }
+        
+        pub fn component_size(&mut self, x: usize) -> usize {
+            let root = self.find(x);
+            self.size[root]
+        }
+    }
 }
